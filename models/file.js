80799b10285fee9ae8a115044da5b31019662357
Kliona Kennet <klionakennet2002@gmail.com>
2025-06-03 22:11:08 +1000

    Updated metadata for database

--- models/file.js
+++ models/file.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const fileSchema = new mongoose.Schema({
   filename: String,
+  storedName: String,
+  token: String,
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 }, { timestamps: true });
 module.exports = mongoose.model('File', fileSchema);
