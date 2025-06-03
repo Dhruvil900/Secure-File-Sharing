47de0418ffaafeb805c51c55e928010d07340325
Kliona Kennet <klionakennet2002@gmail.com>
2025-06-03 22:12:17 +1000

    Error handling

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,10 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
+exports.authenticateToken = async (req, res, next) => {
+  const auth = req.headers['authorization'];
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  req.user = await User.findById(decoded.id).select('-password');
+  next();
 
+};
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,19 @@
 const errorHandler = (err, req, res, next) => {
   console.error(err.stack);
 
+  if (err.name === 'ValidationError') {
+    return res.status(400).json({ 
+      message: 'Validation Error',
+      details: err.message 
+    });
+  }
+
+  if (err.name === 'JsonWebTokenError') {
+    return res.status(401).json({ 
+      message: 'Invalid token' 
+    });
+  }
+
 };
 
 module.exports = errorHandler;
